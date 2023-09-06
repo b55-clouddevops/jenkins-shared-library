df270a4e3ba132b78098f2bdcc11c5c32a441103
CodingManoj <mvp2612@hotmail.com>
2023-09-05 22:41:17 -0400

    Git Auto Push

---
+++ vars/maven.groovy
@@ -0,0 +1,26 @@
+def lintChecks() {
+        sh "echo starting linkChecks for ${COMPONENT}"
+        sh "mvn checkstyle:check || true"
+        sh "echo linkChecks completed for ${COMPONENT}"
+}
+
+def call() {
+    pipeline {
+        agent any 
+        stages {
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        lintChecks()
+                    }
+                }
+            }
+            stage('Generating Artifacts') {
+                steps {
+                    sh "echo Generating Artifiacts...."
+                    sh "npm install"
+                    }
+                }
+            }
+        }
+    }
