e91a910e214d5cbe94a99cdb147fc36ec5f9408d
CodingManoj <mvp2612@hotmail.com>
2023-09-06 22:38:04 -0400

    Git Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -23,8 +23,11 @@ def call() {
             }
             stage('Sonar Checks') {
                 steps {
-                    sh "env"
+                    sh "echo Starting Sonar Checks For ${COMPLETED}"
                     sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
+                    sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
+                    sh "bash qualit-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${COMPONENT}" 
+                    sh "echo ${COMPONENT} Sonar Checks  are completed"
                     }
                 }
             stage('Generating Artifacts') {
