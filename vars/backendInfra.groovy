22b7c835c0d24b36551eaa57ab9c8c14e45a7738
CodingManoj <mvp2612@hotmail.com>
2023-10-04 21:51:05 -0400

    Git Auto Push

---
+++ vars/backendInfra.groovy
@@ -0,0 +1,37 @@
+def call() {
+    properties([
+            parameters([
+                choice(choices: 'dev\nprod', description: "Select your environment", name: "ENV"),
+                choice(choices: 'apply\ndestroy', description: "Chose an action", name: "ACTION"),
+                string(choices: 'APP_VERSION', description: "Enter your backend version", name: "APP_VERSION")
+            ]),
+        ])
+    node {
+        ansiColor('xterm') {
+            git branch: 'main', url: "https://github.com/b55-clouddevops/${REPONAME}.git"
+            
+            stage('terraform init') {
+                sh ''' 
+                    cd ${TFDIR}
+                    ls -ltr
+                    terrafile -f env-${ENV}/Terrafile
+                    terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars  
+                '''       
+            }
+
+            stage('terraform plan') {
+                sh '''
+                    cd ${TFDIR}
+                    terraform plan -var-file=env-${ENV}/${ENV}.tfvars 
+                ''' 
+            }
+
+            stage('Terraform Action') {
+                sh '''
+                    cd ${TFDIR}
+                    terraform ${ACTION} -auto-approve -var-file=env-${ENV}/${ENV}.tfvars
+                '''
+                }
+            }
+        }
+    }
